3cbcbcdbe9f3adf1d5556e9d5b29e7973f91a69a
Andrew Serra <acs8929@rit.edu>
2021-03-06 18:24:58 -0500

    server file has separated contant for the environment variable

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+const MISSING = process.env.MISSING;
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
   while(true) {
-    console.log(`Containers rule! I need more ${process.env.MISSING}`);
+    console.log(`Containers rule! I need more ${MISSING}`);
     await sleep(5000);
   }
 }
